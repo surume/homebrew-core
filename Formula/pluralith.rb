47c9c2785cb3bcb62d15ef60a7d718aa692c67da
surume <sururne@gmail.com>
2023-07-05 04:56:46 +0900

    chore: update Pluralith formula URLs and remove redundant version

--- Formula/pluralith.rb
+++ Formula/pluralith.rb
@@ -3,14 +3,14 @@ class Pluralith < Formula
   homepage "https://pluralith.com"
   url "https://github.com/Pluralith/pluralith-cli/releases/download/v0.2.2/pluralith_cli_tap_darwin_amd64_v0.2.2.tar.gz"
   sha256 "c732e4c93102231d92e6872bce71783df60bc23a340e48cdd01001389627e4d5"
-  version "0.2.2"
+  head "https://github.com/Pluralith/pluralith-cli.git", branch: "master"
 
   on_macos do
     def install
       bin.install "darwin/pluralith"
     end
 
-    if Hardware::CPU.arm?
+		on_arm do
       def caveats
         <<~EOS
           The darwin_arm64 architecture is not supported for the Pluralith
@@ -22,7 +22,7 @@ class Pluralith < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/Pluralith/pluralith-cli/releases/download/v0.2.2/pluralith_cli_tap_linux_amd64_v0.2.2.tar.gz"
       sha256 "6c01db115596774dad5a2445f3091e4c40c999237a1a0e3f0cf1a88fcc70684e"
 
@@ -30,9 +30,5 @@ class Pluralith < Formula
         bin.install "linux/pluralith"
       end
     end
-  end
-
-  test do
-    system "#{bin}/pluralith version"
   end
 end
